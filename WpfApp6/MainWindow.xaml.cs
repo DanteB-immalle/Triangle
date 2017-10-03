500360b537a5f45aaf7d88944f36394b065208ea
Dante Budin <dante.budin@immalle.eu>
2017-10-03 09:27:48 +0200

    Button click = triangle

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -24,5 +24,37 @@ namespace WpfApp6
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                Line Lijn = new Line();
+                Lijn.X1 = 300;
+                Lijn.Y1 = 50;
+                Lijn.X2 = 100;
+                Lijn.Y2 = 50;
+                Lijn.Stroke = new SolidColorBrush(Colors.PeachPuff);
+                MijnGrid.Children.Add(Lijn);
+
+                Line Lijn1 = new Line();
+                Lijn1.X1 = 300;
+                Lijn1.Y1 = 50;
+                Lijn1.X2 = 300;
+                Lijn1.Y2 = 250;
+                Lijn1.Stroke = new SolidColorBrush(Colors.PeachPuff);
+                MijnGrid.Children.Add(Lijn1);
+
+                Line Lijn2 = new Line();
+                Lijn2.X1 = 300;
+                Lijn2.Y1 = 250;
+                Lijn2.X2 = 100;
+                Lijn2.Y2 = 50;
+                Lijn2.Stroke = new SolidColorBrush(Colors.PeachPuff);
+                MijnGrid.Children.Add(Lijn2);
+            }
+
+        }
     }
-}
+
+
+
+
